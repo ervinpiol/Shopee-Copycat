6628d705f1374118a45efff50845056d98ec533a
Ervin Piol <rvnpiol14@gmail.com>
2025-12-28 11:35:02 +0800

    feat: add become seller page

---
+++ frontend/app/become-seller/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Header } from "@/components/header";
+
+export default function Cart() {
+  return (
+    <div className="w-full">
+      <Header />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum quam
+      molestias repudiandae accusamus maxime perferendis iste qui eum
+      dignissimos iure! Recusandae hic minus sunt vitae officia id adipisci eum
+      ad?
+    </div>
+  );
+}
